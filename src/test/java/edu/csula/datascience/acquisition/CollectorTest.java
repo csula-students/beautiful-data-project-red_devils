c9350cda4ccb749b6f50f746792430ec24a2e5fc
Dipesh <dipesh.290193@gmail.com>
2016-04-24 21:29:23 -0700

    Intial commit

--- src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
+++ src/test/java/edu/csula/datascience/acquisition/CollectorTest.java
@@ -1,14 +1,12 @@
 package edu.csula.datascience.acquisition;
 
 import com.google.common.collect.Lists;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collection;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * A test case to show how to use Collector and Source
@@ -25,17 +23,45 @@ public class CollectorTest {
 
     @Test
     public void mungee() throws Exception {
-        List<SimpleModel> list = (List<SimpleModel>) collector.mungee(source.next());
-        List<SimpleModel> expectedList = Lists.newArrayList(
-            new SimpleModel("2", "content2"),
-            new SimpleModel("3", "content3")
-        );
-
+    	List<SimpleModel> list = (List<SimpleModel>) collector.mungee(source.next());     
         Assert.assertEquals(list.size(), 2);
-
-        for (int i = 0; i < 2; i ++) {
-            Assert.assertEquals(list.get(i).getId(), expectedList.get(i).getId());
-            Assert.assertEquals(list.get(i).getContent(), expectedList.get(i).getContent());
-        }
+    }
+    @Test
+    public void checkNull()
+    {
+    	List<SimpleModel> list = (List<SimpleModel>) collector.mungee(source.next()); 
+    	Assert.assertNotNull(list);
+    }
+    @Test
+    public void testNullCSV()
+    {
+    	MockSource src=new MockSource();
+    	List<SimpleModel> list = (List<SimpleModel>) collector.mungee(src.readCSV()); 
+    	Assert.assertNotNull(list);
+    }
+    @Test
+    public void testDataCSV()
+    {
+    	MockSource src=new MockSource();
+    	List<SimpleModel> list = (List<SimpleModel>) collector.mungee(src.readCSV()); 
+    	List<SimpleModel> expectedList=Lists.newArrayList(new SimpleModel("NO", "mail", "No", "A-Marked",
+				"Tranpotation", "NO", "21-707(a)", "blue", "NO", "NO", "NO",
+				"09/30/2014", "description", "CA", "California", "CA", "Yes",
+				"M", "Yes", "location", "Ford", "BMW", "Yes", "NO", "Asian",
+				"CA", "4th distric", "23:51:00", "NO", "Automobile", "NO",
+				"2013"), 
+				new SimpleModel("NO", "mail", "No", "A-Marked",
+				"Tranpotation", "NO", "21-707(a)", "blue", "NO", "NO", "NO",
+				"03/31/2015", "description", "CA", "California", "CA", "Yes",
+				"M", "Yes", "location", "Ford", "BMW", "Yes", "NO", "Asian",
+				"CA", "4th distric", "23:59:00", "NO", "Automobile", "NO",
+				"2013"));
+    	Assert.assertEquals(list.size(), 3);
+    	
+    	for(int i=1;i<3;i++)
+    	{
+    		 Assert.assertEquals(list.get(i).getDate_of_stop(), expectedList.get(i-1).getDate_of_stop());
+             Assert.assertEquals(list.get(i).getTime_of_stop(), expectedList.get(i-1).getTime_of_stop());
+    	}
     }
 }
